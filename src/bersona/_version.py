5c9d13a39037516e2b5e1bc98fbb2e3edb623a0b
Yifan Ren <51114968+fanrenaz@users.noreply.github.com>
2025-10-13 06:20:02 +0000

    chore: bump version to 0.1.1

--- src/bersona/_version.py
+++ src/bersona/_version.py
@@ -3,7 +3,7 @@
 Edit here (or use scripts/bump_version.py) when releasing.
 """
 __all__ = ["__version__", "get_version"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 def get_version() -> str:
     return __version__
